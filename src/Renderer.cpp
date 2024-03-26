b19e61b8168bdfb11dc5ac045adfe8d474edf6a5
tourahi <amine-fb-fb@hotmail.fr>
2024-03-26 01:56:23 +0000

    Draw rect.

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -16,6 +16,45 @@ static struct
   int left, top, right, bottom;
 } clip;
 
+static const char* utf8ToCodepoint(const char *p, unsigned *dst) {
+  unsigned res, n;
+
+  /* 
+   * inspecting the most significant bits of the first byte 
+   * to determine the number of bytes used to encode the character.
+  */ 
+  switch (*p & 0xf0) {
+    case 0xf0 : res = *p & 0x07; n = 3; break;
+    case 0xe0 : res = *p & 0x0f; n = 2; break;
+    case 0xd0 :
+    case 0xc0 : res = *p & 0x1f; n = 1; break;
+    default   : res = *p;        n = 0; break;
+  }
+  // loop over the remaining bytes (if any) to construct the complete code point.
+  while(n--) {
+    res = (res << 6) | (*(++p) & 0x3f);
+  }
+  *dst = res;
+  return p + 1;
+}
+
+static inline Elementxt::Renderer::RColor blendPixel(Elementxt::Renderer::RColor dst, Elementxt::Renderer::RColor src) {
+  int ia = 0xff - src.a;
+  dst.r = ((src.r * src.a) + (dst.r * ia)) >> 8;
+  dst.g = ((src.g * src.a) + (dst.g * ia)) >> 8;
+  dst.b = ((src.b * src.a) + (dst.b * ia)) >> 8;
+  return dst;
+}
+
+static inline Elementxt::Renderer::RColor blendPixel2(Elementxt::Renderer::RColor dst, Elementxt::Renderer::RColor src, 
+  Elementxt::Renderer::RColor color) {
+  src.a = (src.a * color.a) >> 8;
+  int ia = 0xff - src.a;
+  dst.r = ((src.r * color.r * src.a) >> 16) + ((dst.r * ia) >> 8);
+  dst.g = ((src.g * color.g * src.a) >> 16) + ((dst.g * ia) >> 8);
+  dst.b = ((src.b * color.b * src.a) >> 16) + ((dst.b * ia) >> 8);
+  return dst;
+}
 
 typedef struct {
   Elementxt::Renderer::RImage *image;
@@ -37,7 +76,7 @@ struct Elementxt::Renderer::RFont {
 
 
 
-void Elementxt::Renderer::rendererSetClipRect(Elementxt::Renderer::Rect rect) {
+void Elementxt::Renderer::rendererSetClipRect(Elementxt::Renderer::RRect rect) {
   clip.left = rect.x;
   clip.top = rect.y;
   clip.right = rect.x + rect.width;
@@ -48,10 +87,10 @@ void Elementxt::Renderer::rendererInitWindow(SDL_Window *win) {
   assert(win);
   window = win;
   SDL_Surface *surf = SDL_GetWindowSurface(win);
-  Elementxt::Renderer::rendererSetClipRect( (Elementxt::Renderer::Rect) { 0, 0, surf->w, surf->h } );
+  Elementxt::Renderer::rendererSetClipRect( (Elementxt::Renderer::RRect) { 0, 0, surf->w, surf->h } );
 }
 
-void Elementxt::Renderer::rendererUpdateRects(Elementxt::Renderer::Rect *rects, int count) {
+void Elementxt::Renderer::rendererUpdateRects(Elementxt::Renderer::RRect *rects, int count) {
   SDL_UpdateWindowSurfaceRects(window, (SDL_Rect*) rects, count);
   static bool initFrame = true;
   if (initFrame) {
@@ -191,3 +230,77 @@ Elementxt::Renderer::RFont* Elementxt::Renderer::rendererFreeImage(const char* f
 
 }
 
+void Elementxt::Renderer::rendererFreeFont(Elementxt::Renderer::RFont* font) {
+  for (int i = 0; i < MAX_GLYPHSET; i++) {
+    GlyphSet *set = font->sets[i];
+    if (set) {
+      Elementxt::Renderer::rendererFreeImage(set->image);
+      free(set);
+    }
+    free(font->data);
+    free(font);
+  }
+}
+
+void Elementxt::Renderer::rendererSetFontTabWidth(Elementxt::Renderer::RFont* font, int n) {
+  GlyphSet *set = getGlyphset(font, '\t');
+  set->glyphs['t'].xadvance = n;
+}
+
+int Elementxt::Renderer::rendererGetFontTabWidth(Elementxt::Renderer::RFont* font) {
+  GlyphSet *set = getGlyphset(font, '\t');
+  return set->glyphs['t'].xadvance;
+}
+
+int Elementxt::Renderer::rendererGetFontWidth(Elementxt::Renderer::RFont* font, const char* text) {
+  int x = 0;
+  const char *p = text;
+  unsigned codepoint;
+  while(*p) {
+    p = utf8ToCodepoint(p, &codepoint);
+    GlyphSet *set = getGlyphset(font, codepoint);
+    stbtt_bakedchar *g = &set->glyphs[codepoint & 0xff];
+    x += g->xadvance;
+  }
+  return x;
+}
+
+int Elementxt::Renderer::rendererGetFontHeight(Elementxt::Renderer::RFont* font) {
+  return font->height;
+}
+
+
+#define rectDrawLoop(expr)          \
+  for (int j = y1; j < y2; j++) {   \
+    for (int i = x1; i < x2; i++) { \
+      *dPos = expr;                 \
+      dPos++;                       \
+    }                               \
+    dPos += dNextRow;               \
+  }
+
+
+void Elementxt::Renderer::rendererDrawRect(Elementxt::Renderer::RRect rect, Elementxt::Renderer::RColor color) {
+  if (color.a == 0) { return; }
+
+  int x1 = rect.x < clip.left ? clip.left : rect.x;
+  int y1 = rect.y < clip.top ? clip.top : rect.y;
+  int x2 = rect.x + rect.width;
+  int y2 = rect.y + rect.height;
+  x2 = x2 > clip.right ? clip.right : x2;
+  y2 = y2 > clip.bottom ? clip.bottom : y2;
+
+  SDL_Surface *surf = SDL_GetWindowSurface(window);
+  RColor *dPos = (RColor*) surf->pixels;
+  /* Pos(pixel) to start drawing at */
+  dPos += x1 + y1 * surf->w;
+  /* dist to next pixels row */
+  int dNextRow = surf->w - (x2 - x1);
+
+  if (color.a == 0xff) {
+    rectDrawLoop(color);
+  } else {
+    rectDrawLoop(blendPixel(*dPos, color));
+  }
+
+}
--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -15,23 +15,20 @@ namespace Elementxt
     typedef struct { uint8_t r, g, b, a; } RColor;
     typedef struct { int x, y, width, height; } RRect;
 
-    struct Color
-    {
-      uint8_t r, g, b, a;
-    };
-
-    struct Rect
-    {
-      int x, y, width, height;
-    };
-
-    void    rendererSetClipRect(Rect rect);
+    void    rendererSetClipRect(RRect rect);
     void    rendererInitWindow(SDL_Window *win);
-    void    rendererUpdateRects(Rect *rects, int count);
+    void    rendererUpdateRects(RRect *rects, int count);
     void    rendererGetSize(int* w, int* h);
     RImage* rendererNewImage(int width, int height);
     void    rendererFreeImage(RImage *image);
-    RFont*   rendererFreeImage(const char* filename, float size);
+    RFont*  rendererFreeImage(const char* filename, float size);
+    void    rendererFreeFont(RFont *font);
+    void    rendererSetFontTabWidth(RFont *font, int n);
+    int     rendererGetFontTabWidth(RFont *font);
+    int     rendererGetFontWidth(RFont *font, const char *text);
+    int     rendererGetFontHeight(RFont *font);
+
+    void    rendererDrawRect(RRect rect, RColor color);
 
   }
 }
