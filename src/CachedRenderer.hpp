0c5d0212280b460321b480ad2c74dd90c09b161c
tourahi <amine-fb-fb@hotmail.fr>
2024-03-27 01:09:17 +0000

    CachedRenderer.

---
+++ src/CachedRenderer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Renderer.hpp"
+
+namespace CashedRenderer {
+
+  void showDebug (bool enable);
+  void freeFont (Renderer::RFont *font);
+  void setClipRect (Renderer::RRect rect);
+  void drawRect (Renderer::RRect rect, Renderer::RColor color);
+
+  int  drawText(Renderer::RFont *font, const char *text, int x, int y, Renderer::RColor color);
+
+  void invalidate(void);
+  void beginFrame(void);
+  void endFrame(void);
+
+}
